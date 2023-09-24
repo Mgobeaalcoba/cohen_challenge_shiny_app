be026df92b25c6ed797cf7e4a8841f5de97fb5f7
Mariano Gobea Alcoba <gobeamariano@gmail.com>
2023-09-24 10:31:07 -0300

    :art: Add README and improve structure

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# Shiny App with data from TidyTuesday for Cohen Aliados Financieros's challenge
+# Shiny App: Programming Language Analysis
+
+## Overview
+
+This Shiny App is designed to provide insights into programming languages, their origins, and various statistics related to them. It analyzes a dataset of programming languages and their attributes, allowing users to explore and visualize the data interactively.
+
+## Technologies Used
+
+- [R](https://www.r-project.org/): The programming language used for data analysis and Shiny app development.
+- [Shiny](https://shiny.rstudio.com/): A web application framework for R, used to create the interactive user interface.
+- [ggplot2](https://ggplot2.tidyverse.org/): A popular R package for creating data visualizations.
+- [dplyr](https://dplyr.tidyverse.org/): A package for data manipulation and transformation.
+- [openxlsx](https://cran.r-project.org/web/packages/openxlsx/index.html): An R package for reading and writing Excel files.
+
+## Dataset
+
+The dataset used in this Shiny App contains information about programming languages, including their origin years, the number of users, the number of GitHub repositories, and classification types. Here is a breakdown of the dataset's columns:
+
+- `name`: The name of the programming language.
+- `appeared`: The year in which the programming language first appeared.
+- `number_of_users`: The number of users of the programming language.
+- `github_language_repos`: The number of GitHub repositories related to the programming language.
+- `github_language_type`: The classification type of the programming language.
+
+## Features
+
+This Shiny App offers the following features:
+
+1. **Histogram Plot**: Visualize the distribution of programming language appearances over the years. Users can customize the number of bins and filter the data by GitHub language type and category by repositories.
+
+2. **Bar Plot**: Explore the 50 programming languages with the most repositories on GitHub. Users can filter the data by GitHub language type and the last activity registered by the programming language.
+
+3. **About Page**: Learn more about the Shiny App's purpose and development process, including data source and code availability.
+
+4. **Download CSV**: Download the cleaned dataset as a CSV file for further analysis.
+
+## Usage
+
+To use the Shiny App:
+
+1. Ensure you have R installed on your system.
+
+2. Install the required packages using the following commands in your R console:
+
+   ```R
+   install.packages("shiny")
+   install.packages("ggplot2")
+   install.packages("dplyr")
+   install.packages("openxlsx")
+   ```
+
+3. Clone or download the Shiny App code and dataset files from the GitHub repository.
+
+4. Run the Shiny App by opening the R script and executing it. The app will open in your default web browser.
+
+5. Explore and analyze programming language data using the interactive interface.
+
+## Images
+
+<img src="./images/shiny_app_1.png">
+<img src="./images/shiny_app_2.png">
+
+## Author
+
+Mariano Gobea Alcoba
+
+## Acknowledgments
+
+This Shiny App was created as a part of a challenge for Cohen Aliados Financieros and is available on GitHub.
+
+For any questions or inquiries, feel free to contact the author via LinkedIn or GitHub.
--- app.R
+++ app.R
@@ -1,48 +1,44 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
+library(openxlsx)
 
 # Data to be used
 dataset_csv <- read.csv("languages_dataset_cleaning.csv")
 
-# Data filter: programming languages born between 1950 y 2023
+# Data filter: programming languages born between 1950 and 2023
 filtered_data <- dataset_csv %>%
   filter(appeared >= 1950 & appeared <= 2023)
 
-# Ordena el conjunto de datos por "github_language_repos" en orden descendente y selecciona las 20 primeras filas
+# Sort the dataset by "github_language_repos" in descending order and select the top 50 rows
 top_50_languages <- head(arrange(filtered_data, desc(github_language_repos)), 50)
 
-# Voy a quedarme solo con las columnas de number_of_users y github_language_repos que no son nulas para calcular los quartiles:
+# Keep only the columns "number_of_users" and "github_language_repos" that are not null to calculate quartiles
 vector_number_of_users <- na.omit(filtered_data$number_of_users)
-vector_github_language_repos <-
-  na.omit(filtered_data$github_language_repos)
+vector_github_language_repos <- na.omit(filtered_data$github_language_repos)
 
-# Reemplazar NA por "Without classification" en la columna github_language_type
-filtered_data$github_language_type <-
-  ifelse(
-    is.na(filtered_data$github_language_type),
-    "Without classification",
-    filtered_data$github_language_type
-  )
-filtered_data$github_language_repos <-
-  ifelse(
-    is.na(filtered_data$github_language_repos),
-    -1,
-    filtered_data$github_language_repos
-  )
-filtered_data$number_of_users <-
-  ifelse(is.na(filtered_data$number_of_users),
-         -1,
-         filtered_data$number_of_users)
+# Replace NA with "Without classification" in the github_language_type column
+filtered_data$github_language_type <- ifelse(
+  is.na(filtered_data$github_language_type),
+  "Without classification",
+  filtered_data$github_language_type
+)
+filtered_data$github_language_repos <- ifelse(
+  is.na(filtered_data$github_language_repos),
+  -1,
+  filtered_data$github_language_repos
+)
+filtered_data$number_of_users <- ifelse(
+  is.na(filtered_data$number_of_users),
+  -1,
+  filtered_data$number_of_users
+)
 
-# Calcula los cuartiles para number_of_users y github_language_repos
-q_users <-
-  quantile(vector_number_of_users, probs = c(0.25, 0.5, 0.75))
-q_repos <-
-  quantile(vector_github_language_repos, probs = c(0.25, 0.5, 0.75))
+# Calculate quartiles for number_of_users and github_language_repos
+q_users <- quantile(vector_number_of_users, probs = c(0.25, 0.5, 0.75))
+q_repos <- quantile(vector_github_language_repos, probs = c(0.25, 0.5, 0.75))
 
-
-# Función para categorizar por cantidad de usuarios y cantidad de repos
+# Function to categorize by the number of users and number of repositories
 categorize_by_quartiles <- function(value, quartiles) {
   if (value < 0) {
     return("Without info")
@@ -57,38 +53,34 @@ categorize_by_quartiles <- function(value, quartiles) {
   }
 }
 
-# Crea una nueva columna categórica para number_of_users
-filtered_data$category_by_users <-
-  sapply(filtered_data$number_of_users,
-         categorize_by_quartiles,
-         quartiles = q_users)
+# Create a new categorical column for number_of_users
+filtered_data$category_by_users <- sapply(filtered_data$number_of_users,
+                                          categorize_by_quartiles,
+                                          quartiles = q_users)
 
-# Crea una nueva columna categórica para github_language_repos
-filtered_data$category_by_repos <-
-  sapply(filtered_data$github_language_repos,
-         categorize_by_quartiles,
-         quartiles = q_repos)
+# Create a new categorical column for github_language_repos
+filtered_data$category_by_repos <- sapply(filtered_data$github_language_repos,
+                                          categorize_by_quartiles,
+                                          quartiles = q_repos)
 
-# Obtener y visualizar los nombres de las columnas para comprobar que se hayan creado las nuevas columnas:
-# cat(colnames(filtered_data),"\n")
 
-# Define UI for application that draws a histogram
+# Define UI for the application that draws a histogram
 ui <- fluidPage(
   # Application title
   titlePanel("Programming language origin by year"),
   
-  #Giving a tabset appearance to the app
+  # Giving a tabset appearance to the app
   tabsetPanel(
     type = "tabs",
-    #Each tabPanel call specifies input for contents of tab
-    tabPanel("Hist plot", #Tab title
+    # Each tabPanel call specifies input for contents of tab
+    tabPanel("Hist plot", # Tab title
              
-             # Sidebar with a slider input for number of bins
+             # Sidebar with a slider input for the number of bins
              sidebarLayout(
                sidebarPanel(
                  sliderInput(
                    inputId = "bins",
-                   label = "Quantity bins:",
+                   label = "Number of bins:",
                    min = 1,
                    max = 73,
                    value = 10
@@ -110,9 +102,9 @@ ui <- fluidPage(
                ),
                mainPanel(plotOutput("distPlot"))
              )),
-    tabPanel("Bar plot", #Tab title
+    tabPanel("Bar plot", # Tab title
              
-             # Sidebar with a slider input for number of bins
+             # Sidebar with a slider input for the number of bins
              sidebarLayout(
                sidebarPanel(
                  selectInput(
@@ -134,35 +126,38 @@ ui <- fluidPage(
                mainPanel(plotOutput("barPlot"))
              )),
     tabPanel("About", 
-             p(HTML("This is a Shiny Application built to a Cohen Aliados Financieros's challenge by Mariano Gobea Alcoba.")),
+             p(HTML("")),
+             p(HTML("This is a Shiny Application built for a Cohen Aliados Financieros challenge by Mariano Gobea Alcoba.")),
              p(HTML("To prepare it, first select a dataset of my interest among those available in TidyTuesday and I have downloaded it to my project using the code written in download_dataset.R")),
-             p(HTML("Once I had the dataset in my possession, I went through a process of debugging and cleaning it in order to remove junk information. The mime can be observed in cleaning_dataset.R")),
-             p(HTML("Then I decided to do a brief and summarized EDA in order to know my dataset, its main columns and number of rows, how much null or n/a data I had in each of them. It can be found in eda.R")),
-             p(HTML("Code for the app is available on my <a href= https://github.com/Mgobeaalcoba/cohen_challenge_shiny_app>Github</a>")),
+             p(HTML("Once I had the dataset in my possession, I went through a process of debugging and cleaning it to remove junk information. The code can be observed in cleaning_dataset.R")),
+             p(HTML("Then I decided to perform a brief and summarized EDA to understand my dataset, its main columns, and the number of rows, as well as the amount of null or N/A data in each of them. The code can be found in eda.R")),
+             p(HTML("The code for the app is available on my <a href= https://github.com/Mgobeaalcoba/cohen_challenge_shiny_app>Github</a>")),
              p(HTML("I set out to answer the following two questions in this Shiny App:")),
              p(HTML("1. What are the 10 most used programming languages?")),
-             p(HTML("2. How many programming languages were created each year? And what was the most fruitful year?")),
-             p(HTML("</br>For any questions or inquiries, you can find me at <a href=https://www.linkedin.com/in/mariano-gobea-alcoba//> my LinkedIn profile</a> or on <a href = https://github.com/Mgobeaalcoba>my Github profile</a>."))
+             p(HTML("2. How many programming languages were created each year? And what was the most productive year?")),
+             p(HTML("</br>For any questions or inquiries, you can find me on <a href=https://www.linkedin.com/in/mariano-gobea-alcoba//> my LinkedIn profile</a> or on <a href = https://github.com/Mgobeaalcoba>my Github profile</a>.")),
+             p(HTML(""))
     )
     
-  )
+  ),
+  # Add a download button for the CSV file
+  downloadButton("downloadCSV", "Download CSV (language_dataset_cleaning.csv)")
 )
-
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   output$distPlot <- renderPlot({
-    #The "Selected" variables will serve to subset out data in function of
-    # the input: they are a way of storing the input selected
+    # The "Selected" variables will serve to subset our data based on user input;
+    # they store the user-selected input.
     BINSelected <- input$bins
     GLTYPESelected <- input$github_language_type
     CBREPOSelected <- input$category_by_repos
     
-    # generate bins based on input$bins from ui.R
+    # Generate bins based on input$bins from ui.R
     x    <- filtered_data$appeared
     bins <- seq(min(x), max(x), length.out = BINSelected + 1)
     
-    # generate a subset for interactive filter:
+    # Generate a subset for interactive filtering
     hist_data <-
       subset(
         filtered_data,
@@ -181,20 +176,20 @@ server <- function(input, output) {
       ) +
       xlab('Appeared year') +
       ylab('Frequency') +
-      ggtitle('Histogram of programming language appeared') +
+      ggtitle('Histogram of programming language appearances') +
       theme_minimal() +
-      scale_x_continuous(breaks = seq(min(bins), max(bins), by = 5))  # Establece las marcas del eje X
+      scale_x_continuous(breaks = seq(min(bins), max(bins), by = 5))
     
     print(p)
   })
   
   output$barPlot <- renderPlot({
-    #The "Selected" variables will serve to subset out data in function of
-    # the input: they are a way of storing the input selected
+    # The "Selected" variables will serve to subset our data based on user input;
+    # they store the user-selected input.
     GLTYPESelected <- input$github_language_type2
     APPEAREDSelected <- input$appeared
     
-    # generate a subset for interactive filter:
+    # Generate a subset for interactive filtering
     hist_data2 <-
       subset(
         top_50_languages,
@@ -202,22 +197,33 @@ server <- function(input, output) {
           appeared <= APPEAREDSelected
       )
     
-    # Crear un gráfico de barras utilizando ggplot2
+    # Create a bar chart using ggplot2
     q <- ggplot(hist_data2, 
                 aes(x = reorder(title, -github_language_repos), 
                     y = github_language_repos)) +
       geom_bar(stat = "identity", 
                fill = "darkgray") +
-      xlab("Lenguaje de Programación") +
-      ylab("Número de Repositorios en GitHub (en millones)") +
-      ggtitle("50 Lenguajes de Programación con más Repositorios en GitHub") +
+      xlab("Programming Language") +
+      ylab("Number of Repositories on GitHub (in millions)") +
+      ggtitle("50 Programming Languages with the Most Repositories on GitHub") +
       theme(axis.text.x = element_text(angle = 45, hjust = 1), 
-            panel.background = element_rect(fill = "white")) +  # Rotar etiquetas del eje x si es necesario  # Rotar etiquetas del eje x si es necesario
-      scale_y_continuous(labels = scales::comma_format(scale = 1e-6)) 
-    
+            panel.background = element_rect(fill = "white")) +
+      scale_y_continuous(labels = scales::comma_format(scale = 1e-6))
     
     print(q)
   })
+  
+  # Function to download the CSV file when the button is clicked
+  output$downloadCSV <- downloadHandler(
+    filename = function() {
+      "language_dataset_cleaning.csv"  # Name of the downloaded file
+    },
+    content = function(file) {
+      # Copy the file from its location in the project
+      file.copy("./languages_dataset_cleaning.csv", file)
+    }
+  )
+  
 }
 
 # Run the application
